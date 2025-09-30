189ed6b870b50d830a8744cc078342cf1b33d186
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-09-30 19:01:04 +0530

    datas added in user.json

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const userRoutes = require("./routes/userRoutes");
+
+// const app = express();
+// app.use(bodyParser.json());
+
+// app.use("/api/users", userRoutes);
+
+// const PORT = 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const express = require("express");
-const bodyParser = require("body-parser");
-const userRoutes = require("./routes/userRoutes");
-
-const app = express();
-app.use(bodyParser.json());
-
-app.use("/api/users", userRoutes);
-
+const api = express();
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+api.get("/", (req, res) => {
+  res.send("Hello Express!");
+});
+api.get("/users", (req, res) => {
+  res.send(users);
+});
+api.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).json({ error: "User not found" });
+  res.send(user);
+});
+api.post("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).json({ error: "User not found" });
+  res.send(user);
+});
+api.put("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (!user) return res.status(404).json({ error: "User not found" });
+  res.send(user);
+});
+api.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,36 +1,6 @@
-const { error } = require("console");
 const fs = require("fs");
-const path = require("path");
+const filePath = require("../data/user.json");
 
-const filePath = path.join(__dirname, "../data/users.json");
-
-// // Get all users
-// exports.getAllUsers = (req, res) => {
-//   // Read all users from the file
-//   const users = readUsersFromFile();
-
-//   // Send the list of users as JSON
-//   res.json(users);
-// };
-
-// // Get user by ID
-// exports.getUserById = (req, res) => {
-//   // Read all users from the file
-//   const users = readUsers();
-
-//   // Convert id from string to number
-//   const id = parseInt(req.params.id);
-
-//   // Find the user with matching id
-//   const user = users.find((u) => u.id === id);
-
-//   // If no user found, return 404
-//   if (!user) {
-//     return res.status(404).json({ message: "User not found" });
-//   }
-
-//   res.json(user);
-// };
 fs.readFile(filePath, "utf-8", (err, data) => {
   if (!data) return resizeBy.status(404).json({ error: "User not found" });
 });
@@ -41,3 +11,14 @@ fs.writeFile(filePath, content, "utf8", (err) => {
   }
   res.status(200).send("User added successfully.");
 });
+
+exports.getAllusers = (req, res) => {
+  res.json(filePath);
+};
+exports.getuserbyID = (req, res) => {
+  const userID = req.params.id;
+  const userName = req.params.name;
+  const userEmail = req.params.email;
+  const user = { id: userID, name: userName, email: userEmail };
+  res.json(users.push(user));
+};
