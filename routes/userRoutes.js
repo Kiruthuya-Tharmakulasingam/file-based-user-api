e19f8f2baac6d2de03530d534353cda5098b24f7
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-02 18:47:32 +0530

    routes added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,18 @@
-const express = require("express");
+import express from "express";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
+
 const router = express.Router();
-const userController = require("../controllers/userController");
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
-module.exports = router;
+export default router;
