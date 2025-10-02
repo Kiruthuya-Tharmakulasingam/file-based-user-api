50b6cabbe14736342775e6b2e89c6eb7546bc4ac
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-02 20:51:26 +0530

    get method added

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,11 +10,38 @@ const __dirname = path.dirname(__filename);
 const dataFile = path.join(__dirname, "../data/users.json");
 
 // Read users
-const readUsers = () => {
-  return fs.readFile(dataFile, "utf-8").then((data) => JSON.parse(data));
+const readUsers = async () => {
+  const data = await fs.readFile(dataFile, "utf-8");
+  return JSON.parse(data);
 };
 
 // Write users
-const writeUsers = (users) => {
-  return fs.writeFile(dataFile, JSON.stringify(users, null, 2));
+const writeUsers = async (users) => {
+  await fs.writeFile(dataFile, JSON.stringify(users, null, 2));
+};
+// Get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await readUsers();
+    res.status(200).json(users);
+  } catch {
+    res.status(500).json({ message: "Cannot read user list" });
+  }
+};
+
+// Get one user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const users = await readUsers();
+    const userId = parseInt(req.params.id);
+    const user = users.find((u) => u.id === userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch {
+    res.status(500).json({ message: "Error getting user" });
+  }
 };
