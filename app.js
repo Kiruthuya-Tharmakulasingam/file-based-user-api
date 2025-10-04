e5f76b31d62b2a64710eaa877d585ac3da0c2686
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-10-04 21:11:01 +0530

    port change

--- app.js
+++ app.js
@@ -3,14 +3,8 @@ import bodyParser from "body-parser";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-const PORT = 3000;
-
 app.use(bodyParser.json());
-
-// all routes inside userRoutes will use this base path
 app.use("/api/users", userRoutes);
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
